19e176f1c5d1d5520d90015bdd14dee60bc804a8
marrufovic <marrufovic@gmail.com>
2015-04-15 23:40:50 +0000

    add images folder

--- solitaireView.js
+++ solitaireView.js
@@ -23,13 +23,37 @@
 
 })(window);
 
-    function buildBoard(){
-	var output = "<div id='deck'>deck</div><div id='waste'>waste</div><div id='dest1'>destination 1</div><div id='dest2'>destination 2</div><div id='dest3'>destination 3</div><div id='dest4'>destination 4</div><div id='piles'><div id='pile1'>pile 1</div><div id='pile2'>pile 2</div><div id='pile3'>pile 3</div><div id='pile4'>pile 4</div><div id='pile5'>pile 5</div><div id='pile6'>pile 6</div><div id='pile7'>pile 7</div>";
-	  
-	document.getElementById('solitaireBoard').innerHTML = output;
-    }
-  
-function deck(array){
+function buildBoard(){
+    var output = "<div id='deck'>deck</div>"
+      +"<div id='waste'>waste</div>"
+      +"<div id=dest>"
+      +"<div id='dest1'>destination 1</div>"
+      +"<div id='dest2'>destination 2</div>"
+      +"<div id='dest3'>destination 3</div>"
+      +"<div id='dest4'>destination 4</div>"
+      +"</div>"
+      +"<div id='piles'>"
+      +"<div id='pile1'>pile 1</div>"
+      +"<div id='pile2'>pile 2</div>"
+      +"<div id='pile3'>pile 3</div>"
+      +"<div id='pile4'>pile 4</div>"
+      +"<div id='pile5'>pile 5</div>"
+      +"<div id='pile6'>pile 6</div>"
+      +"<div id='pile7'>pile 7</div>"
+      +"</div>";
+      
+    document.getElementById('solitaireBoard').innerHTML = output;
+    
+    deck();
+    waste();
+}
+
+function deck(){
+    var cardToDisplay = "<img src='images/backOfCard.jpg' width='110' height='130' alt='deck'/>";
+    document.getElementById('deck').innerHTML = cardToDisplay;
+}
+
+function waste(){
     var cardToDisplay = array[array.length - 1];
     document.getElementById('deck').innerHTML = cardToDisplay;
-   }
+}
