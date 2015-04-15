01c99f64e24af6837c19865b8e4339284a36c90d
marrufovic <marrufovic@gmail.com>
2015-04-15 22:17:31 +0000

    moving file

--- solitaireView.js
+++ solitaireView.js
@@ -19,12 +19,6 @@
 	}
 
 	window.SolitaireView = SolitaireView;
- 
-
-function deck(array){
-    var cardToDisplay = array[array.length - 1];
-    document.getElementById('deck').innerHTML = cardToDisplay;
-}
 
 
 })(window);
@@ -33,4 +27,9 @@ function deck(array){
 	var output = "<div id='deck'>deck</div><div id='waste'>waste</div><div id='dest1'>destination 1</div><div id='dest2'>destination 2</div><div id='dest3'>destination 3</div><div id='dest4'>destination 4</div><div id='piles'><div id='pile1'>pile 1</div><div id='pile2'>pile 2</div><div id='pile3'>pile 3</div><div id='pile4'>pile 4</div><div id='pile5'>pile 5</div><div id='pile6'>pile 6</div><div id='pile7'>pile 7</div>";
 	  
 	document.getElementById('solitaireBoard').innerHTML = output;
-}
+    }
+  
+function deck(array){
+    var cardToDisplay = array[array.length - 1];
+    document.getElementById('deck').innerHTML = cardToDisplay;
+   }
