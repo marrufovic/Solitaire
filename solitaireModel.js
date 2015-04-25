1d5a4f82a06def99b176dc8c707aa8e583c32580
Elliot Hatch <elliot.hatch@gmail.com>
2015-04-25 13:54:22 -0600

    Added test framework, fixed undefined rule bug

--- solitaireModel.js
+++ solitaireModel.js
@@ -125,12 +125,16 @@
 	SolitaireModel.prototype.canGrabCard = function(card)
 	{
 		var grabRules = this.game.rules.pileTypes[card.pile.pileType].grab;
+		if(typeof grabRules === 'undefined')
+			return true;
 		return this._evaluateRule(grabRules, { grabTarget : card });
 	};
 
 	SolitaireModel.prototype.canDropCard = function(card, pile, pos)
 	{
 		var dropRules = this.game.rules.pileTypes[pile.pileType].drop;
+		if(typeof dropRules === 'undefined')
+			return true;
 		var dropTarget = pile.peekCard(pos);
 		return this._evaluateRule(dropRules, { held : card, pile : pile, dropTarget : dropTarget });
 	};
