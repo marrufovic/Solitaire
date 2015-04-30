85fad69abc88e3d835f4d938eb1ac47e03f5f194
jameslundgren <jameslundgren@hotmail.com>
2015-04-30 02:08:42 +0000

    cards snap to columns, see todo comments for needed fixes

--- viewCopy.js
+++ viewCopy.js
@@ -70,40 +70,17 @@
 	//	        renderer.view.style.height= "400px";
 	requestAnimFrame( animate );
 	
-
-	// still need to amek the rectangles resize
-	function rectangle( x, y, width, height, backgroundColor, borderColor, borderWidth ) { 
-	    var box = new PIXI.Graphics();
-	    box.beginFill(backgroundColor);
-	    box.lineStyle(borderWidth , borderColor);
-	    box.drawRect(0, 0, width - borderWidth, height - borderWidth);
-	    box.endFill();
-	    box.position.x = x  ;
-	    box.position.y = y  ;
-
-	    box.scale.x = box.scale.y = (window.innerWidth + window.innerHeight)/2000;
-	    return box;
-	};
-
-
-	// create a 100x100 white rectangle with a 10px black border at position 10/10
-	stage.addChild(rectangle(window.innerHeight *.7 , window.innerHeight * .03 , 90, 110 , 0xFFFFFF, 0x000000, 5));
-	stage.addChild(rectangle(window.innerHeight *.9 ,window.innerHeight * .03 , 90, 110 , 0xFFFFFF, 0x000000, 5));
-	stage.addChild(rectangle(window.innerHeight *1.1,window.innerHeight * .03  , 90, 110 , 0xFFFFFF, 0x000000, 5));
-	stage.addChild(rectangle(window.innerHeight *1.3,window.innerHeight * .03  , 90, 110 , 0xFFFFFF, 0x000000, 5));
 	
-	
-	var check = true;
 	var current_pile;
 	var y_offset = 1.0; 
 	var x_offset = 1.0; 
+
 	// Pass in the image that is associated with the card
 	//SolitaireView.prototype.createCard = function(x, y, texture, pile_id, facing_up)
 	SolitaireView.prototype.createCard = function(card_obj, texture_start, texture_alternate)
 	{
 	    
 	    //console.log(card_obj); 
-
 	    var facing_up = card_obj.facingUp; 
 	    var pile_id = card_obj.pile.pileId;
 	    
@@ -150,7 +127,7 @@
 	    }
 	    
 	    if(y === 0)
-		y = window.innerHeight * .1;
+		y = window.innerHeight * .1 
 	    else 
 		y = window.innerHeight * y_offset * .3;
 
@@ -168,7 +145,9 @@
 	    card.anchor.y = 0.5;
 	    // make it a bit bigger, so its easier to touch
 	    card.scale.x = card.scale.y = (window.innerWidth + window.innerHeight)/2000;
-	    
+	
+
+
 	    // use the mousedown and touchstart
 	    card.mousedown = card.touchstart = function(data)
 	    {
@@ -181,9 +160,7 @@
 		
 		if (card_obj.pile.pileId === "stock1")
 		{
-		    // The waste pile is inbetween 1/8 and 2/8 the screen width...may need to adjust if we choose to have 3 cards dealt
-		    // from the stock pile
-		    card.position.x = (3/16) * window.innerWidth;  
+		    card.position.x += 100;  
 		    card.setTexture(alt_text); 
 		}
 
@@ -210,20 +187,17 @@
 		
 	    };
 
+
+	    var isIntersecting = function(c1, c2)
+	    {
+		return !(c2.x > (c1.x + c1.width) || (c2.x + c2.width) < c1.x || c2.y > (c1.y + c1.height) || (c2.y + c2.height) <c1.y); 
+	    }
+
+
 	    // set the events for when the mouse is released or a touch is released
 	    card.mouseup = card.mouseupoutside = card.touchend = card.touchendoutside = function(data)
 	    {
 
-		// TODO: 
-		// -account for the Y value of the card
-		// -this potentially doesn't work with some versions of solitaire, would work for klondike (7 columns)
-		// -the x value for a card isn't getting reset. It moves if it is within the bounds of the column, but if it is outside of it
-		//  then it goes back to the position it was created in (its original x value, not the last x value it was at)
-		// -need to account for the stockpile and if a card is dragged to the upper slots (I think we call them winpiles..)
-		//  e.g. if we drag from the stockpile to the lower columns vs just clicking on the stockpile to deal new cards. 
-		
-
-		// This breaks up the board into different columns and gives each column a width in which the card will snap to the center of the column (+- .5*card.width)
 		var col;
 		if(card.position.x > ((1/8)*window.innerWidth - (.5*card.width)) && card.position.x < ((1/8)*window.innerWidth + (.5*card.width)))
 		{
@@ -257,49 +231,22 @@
 		{
 		    col = -1;  
 		}    
-
 		
-
-		// Would be -1 if it was from stockpile or if when the card was released, it wasn't in any of the columns
-		// We need to account for if the user drags from the stockpile onto one of the columns. As it is right now 
-		// it just snaps it to the 
+		
+		this.alpha = 1
+		this.dragging = false;
+		
 		if (col === -1)
-		{
-		    if (card_obj.pile.pileId === "stock1")
-			card.position.x = (3/16) * window.innerWidth;
-		    else 
-			card.position.x = x;		
+		    card.position.x = x;
 		
-		}
-		else
+		else if(card_obj.pile.pileId !== "stock1")
 		{
 		    console.log(col);
 		    card.position.x = (col/8)*window.innerWidth; 
 		    card.position.y = y;
 		}
-
-
-		this.alpha = 1
-		this.dragging = false; 
-		//var pileType = card.pile.pileType;
 		
-		// 
-		if (model.canDropCard(card_obj, card_obj.pile, 1))
-		{
-		    //alert('here');
-		}
-		else
-		{
-		    //console.log(card_obj.pile);
-		    //card.position.x = x;
-		    //card.position.y = y;
-		}
 		
-		//	if(card.pile.pileId !== "stock1")
-		//	{
-		//	    card.position.x = x;
-		//	    card.position.y = y;
-		//	}
 		// set the interaction data to null
 		this.data = null;
 	    };
@@ -322,7 +269,7 @@
 	    //------------------------------------------------------------
 	    // This is only being called on the last object being created...
 	    // need to fix this so the method is called on all card objects. 
-	    window.onresize = resize;
+	    // window.onresize = resize;
 	    //------------------------------------------------------------
 
 
@@ -346,101 +293,8 @@
 	}
 	
 	this.piles = {};
-	
 	this.onCardMoved = null;
 	this.onCardActivated = null;
-
-
-
-        /**
-	 * resize the c4 sprite so that it takes up most of the screen
-	 *
-	 */
-	function resize (event)
-	{
-	    
-	    console.log("--------------------");
-	    //console.log("  Window Size: " + window.innerWidth + ", " + window.innerHeight);
-	    //console.log("  C4 current    x,y, w,h  " + c4.position.x + ", " + c4.position.y + ": "+ c4.width   + ", " + c4.height);
-	    
-	    //  boundingbox.clear();
-	    //boundingbox.lineStyle(2,0xffffff);
-	    //boundingbox.beginFill(0xFFFF0B, 0);
-	    //boundingbox.drawRect(0,0,window.innerWidth, window.innerHeight);
-	    //boundingbox.endFill();
-	    
-	    
-	    //
-	    // Resize Renderer Window
-	    //
-	    renderer.resize(window.innerWidth, window.innerHeight);
-
-	    //
-	    // find current c4 bounds and resize/reposition c4 board
-	    //
-	    //    var c4_bounds = PIXI.DisplayObjectContainer.prototype.getBounds.call(c4).clone();
-	    
-	    //
-	    // need to compute new scale based on original size of C4 sprite, so 
-	    // put back to original size
-	    //
-	    
-	    stage.scale.x = 1;  // WARNING: must compute center based on original size, not previous scaled size
-	    stage.scale.y = 1;
-	    
-	    var dw = (window.innerWidth-100)/stage.width; //stage_bounds
-	    var dh = (window.innerHeight-100)/stage.height; // stage_bounds
-	    var dm = Math.min(dw,dh);
-	    
-	    stage.scale.x = dm;
-	    stage.scale.y = dm;
-	    
-	    //
-	    // find current stage bounds and resize/reposition stage board
-	    //
-	    //    var stage_bounds = PIXI.DisplayObjectContainer.prototype.getBounds.call(stage).clone();
-	    stage.position.x = (window.innerWidth/2) - (stage.width/2) +100 ;
-	    stage.position.y = (window.innerHeight/2) - (stage.height/2) -15;
-	    
-	    
-	    
-	    //    renderer.render(stage);     // render the stage (required to recompute the acutal size of the sprite)
-	    //    stage_bounds = PIXI.DisplayObjectContainer.prototype.getBounds.call(stage).clone();
-	    
-	    
-	    //stage.position.x = 0;
-	    //stage.position.y = 0;
-	    
-	    //    stage.updateTransform();
-	    
-	    //    renderer.render(stage);     // render the stage (required to recompute the acutal size of the sprite)
-	    //    stage_bounds = PIXI.DisplayObjectContainer.prototype.getBounds.call(stage).clone();
-	    
-	    //    console.log("  STAGE bounds x,y, w,h  " + stage_bounds.x + ", " + stage_bounds.y + ": "+ stage_bounds.width   + ", " + stage_bounds.height);
-	    //	console.log("  STAGE final  x,y, w,h  " + stage.position.x + ", " + stage.position.y + ": "+ stage.width   + ", " + stage.height);
-	    
-	    //	console.log("   scale: " + dw + ", " + dh);
-	    
-	    
-	    //stage_bounds = PIXI.DisplayObjectContainer.prototype.getBounds.call(stage).clone();
-	    //console.log("  STAGE final bounds x,y, w,h  " + stage_bounds.x + ", " + stage_bounds.y + ": "+ stage_bounds.width   + ", " + stage_bounds.height);
-	    
-	    //    boundingbox.position.x = stage.position.x;
-	    //    boundingbox.position.y = stage.position.y;
-	    
-	    //    boundingbox.clear();
-	    //    boundingbox.lineStyle(2,0xffffff);
-	    //    boundingbox.beginFill(0xFFFF0B, .1);
-	    //    boundingbox.drawRect(stage_bounds.x, stage_bounds.y, stage_bounds.width, stage_bounds.height);
-	    //    boundingbox.endFill();
-	    
-	    
-	    stage.updateTransform();
-	    //renderer.clearBeforeRender = true;
-	    //stage.dirty = true;
-
-	};
-
     };
 
 
@@ -519,6 +373,51 @@
 	// this.card.pile.getCardPosition(this.card) - returns index of what card position is of the card on the pile 
 	// 0 is bottom card, top is length -1 
 	solitaireView.createCard(this.card, start_texture, alternate_texture); 
+
+
+	
+	/**
+	 * resize the card sprite 
+	 *
+	 */
+	/*
+	function resize (event, card, stage)
+	{
+	    
+	    console.log("--------------------");
+	    
+	    //
+	    // Resize Renderer Window
+	    //
+	    renderer.resize(window.innerWidth, window.innerHeight);
+	    
+	    // need to compute new scale based on original size of C4 sprite, so 
+	    // put back to original size
+	    //
+	    
+	    card.scale.x = 1;  // WARNING: must compute center based on original size, not previous scaled size
+	    card.scale.y = 1;
+	    
+	    var dw = (window.innerWidth-100)/card.width; //card_bounds
+	    var dh = (window.innerHeight-100)/card.height; // card_bounds
+	    var dm = Math.min(dw,dh);
+	    
+	    card.scale.x = dm;
+	    card.scale.y = dm;
+	    
+	    //
+	    // find current card bounds and resize/reposition card board
+	    //
+	    //    var card_bounds = PIXI.DisplayObjectContainer.prototype.getBounds.call(card).clone();
+	    card.position.x = (window.innerWidth/2) - (card.width/2) +100 ;
+	    card.position.y = (window.innerHeight/2) - (card.height/2) -15;
+	    
+	    
+	    stage.updateTransform();
+	    
+	};
+	*/
+	
         // solitaireView.createCard(this.card.pile.position.x, this.card.pile.position.y, texture, this.card.pile.pileId, this.card.facingUp); 
     };
 
